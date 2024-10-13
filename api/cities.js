243f915a7c37a7cb55b52bb65ea76e16eb46b877
Ryan Adi Prasetyo <ryanadiprasetyo2004@gmail.com>
2024-10-13 20:23:55 +0700

    fix: insert data to db from client

--- api/cities.js
+++ api/cities.js
@@ -14,7 +14,7 @@ router.post("/", async (req, res) => {
   try {
     const { city_name } = req.body;
     const result = await insert(city_name);
-    res.json(result);
+    res.status(201).json(result);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
